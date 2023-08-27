5ec77bba3a2872601db619270b07cf3ec6830270
Adrian Miarczyński <adrian331144@gmail.com>
2023-08-27 22:59:54 +0200

    finish task

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,4 +1,5 @@
 import express from "express";
+import Joi from "joi";
 
 import {
   addContact,
@@ -9,6 +10,12 @@ import {
 } from "../../models/contacts.js";
 
 export const router = express.Router();
+
+const schema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string().required(),
+});
 
 router.get("/", async (req, res, next) => {
   try {
@@ -40,10 +47,14 @@ router.get("/:id", async (req, res, next) => {
 router.post("/", async (req, res, next) => {
   const data = req.body;
   try {
+    const { error } = schema.validate(data);
+    if (error) {
+      return res.status(400).json(error.message);
+    }
     const contact = await addContact(data);
     res.json({
       status: "success",
-      code: 200,
+      code: 201,
       data: { contact },
     });
   } catch (err) {
@@ -53,7 +64,6 @@ router.post("/", async (req, res, next) => {
 
 router.delete("/:id", async (req, res, next) => {
   const { id } = req.params;
-  console.log(id);
   try {
     const isRemoved = await removeContact(id);
     if (!isRemoved) {
@@ -75,9 +85,12 @@ router.delete("/:id", async (req, res, next) => {
 
 router.put("/:id", async (req, res, next) => {
   const { id } = req.params;
-  console.log(id);
   const body = req.body;
   try {
+    const { error } = schema.validate(body);
+    if (error) {
+      return res.status(400).json(error.message, "missing fields");
+    }
     const contactUpdate = await updateContact(id, body);
     res.json({
       status: "success",
@@ -85,6 +98,6 @@ router.put("/:id", async (req, res, next) => {
       data: { contactUpdate },
     });
   } catch (err) {
-    req.status(500).json(err);
+    req.status(404).json(err, "Not found");
   }
 });
